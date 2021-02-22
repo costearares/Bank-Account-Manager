ce8e53d7e282c89557212b13a2177514e7e2a4b3
Rares <rarescostea20@yahoo.com>
2021-02-22 21:29:51 +0200

    Add User

---
+++ src/main/java/com/bank/account/manager/model/User.java
@@ -0,0 +1,47 @@
+package com.bank.account.manager.model;
+
+public class User {
+    private String id;
+    private String name;
+    private String password;
+    private double balance;
+
+    public User(String id, String name, String password, double balance) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
